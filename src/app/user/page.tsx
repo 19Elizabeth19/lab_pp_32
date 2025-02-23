11290fa995c3e2da7af4a3ae0563d3af77a2646a
19Elizabeth19 <liza.mineeva.03@bk.ru>
2025-02-23 20:37:02 +0600

    lec2

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,21 +1,18 @@
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 
-import { Navbar } from "./_components/navbar";
-import { SigninLink } from "./_components/signlink";
+import { Navbar } from "./..//_components/navbar";
+import { SigninLink } from "./..//_components/signlink";
 
 export default async function Home() {
   const session = await auth();
-  // if (session?.user) {
-  //   void api.post.getLatest.prefetch();
-  // }
   return (
     <HydrateClient>
       <header>
         {session ? <Navbar session={session} /> : <SigninLink />}
       </header>
       <main>
-        <h1> Main page</h1>
+        <h1> User page</h1>
       </main>
     </HydrateClient>
   );
