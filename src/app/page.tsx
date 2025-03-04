e8cbaa9e1ed9d391d5d55bb0cd6d00320cf56001
19Elizabeth19 <liza.mineeva.03@bk.ru>
2025-03-04 18:38:09 +0600

    lec3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,9 @@
-import { auth } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
+// import { auth } from "~/server/auth";
+// import { api, HydrateClient } from "~/trpc/server";
 
-import { Navbar } from "./_components/navbar";
-import { SigninLink } from "./_components/signlink";
+// import { Navbar } from "./_components/navbar";
+// import { SigninLink } from "./_components/signlink";
 
 export default async function Home() {
-  const session = await auth();
-  // if (session?.user) {
-  //   void api.post.getLatest.prefetch();
-  // }
-  return (
-    <HydrateClient>
-      <header>
-        {session ? <Navbar session={session} /> : <SigninLink />}
-      </header>
-      <main>
-        <h1> Main page</h1>
-      </main>
-    </HydrateClient>
-  );
+  return <h1> Main page</h1>
 }
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,19 +1,20 @@
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 
-import { Navbar } from "./..//_components/navbar";
-import { SigninLink } from "./..//_components/signlink";
+// import { Navbar } from "./..//_components/navbar";
+// import { SigninLink } from "./..//_components/signlink";
+import { db } from "~/server/db";
+import { AddUser } from "../_components/user/addUser";
 
 export default async function Home() {
-  const session = await auth();
+  const users = await db.user.findMany();
+
   return (
-    <HydrateClient>
-      <header>
-        {session ? <Navbar session={session} /> : <SigninLink />}
-      </header>
-      <main>
-        <h1> User page</h1>
-      </main>
-    </HydrateClient>
-  );
+    <>
+      <h1>User page</h1>
+      <AddUser />
+      {users.map((user) => <div key={user.id}>{user.email}</div>)}
+    </>
+  )
+
 }
